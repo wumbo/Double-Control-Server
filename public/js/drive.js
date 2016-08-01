4be33fd707564e5c1b6b01d328c7610c4a66c04d
Simon Crequer <sjc291@uclive.ac.nz>
2016-08-01 15:20:09 +1200

    Add keyboard controls

--- public/js/drive.js
+++ public/js/drive.js
@@ -1,10 +1,41 @@
 $(function(){
 
-var socket = io(':8443');
+window.socket = io(':8443');
 
 socket.on('connect', function() {
     console.log('Connected to socket');
     socket.emit('send doubles');
+$(window).bind('keydown', function(e){
+    if (e.keyCode == 37) {
+        console.log('left');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), left: 'start'});
+    } else if (e.keyCode == 38) {
+        console.log('up');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), forward: 'start'});
+    } else if (e.keyCode == 39) {
+        console.log('right');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), right: 'start'});
+    } else if (e.keyCode == 40) {
+        console.log('down');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), backward: 'start'});
+    }
+});
+
+$(window).bind('keyup', function(e){
+    if (e.keyCode == 37) {
+        console.log('left');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), left: 'stop'});
+    } else if (e.keyCode == 38) {
+        console.log('up');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), forward: 'stop'});
+    } else if (e.keyCode == 39) {
+        console.log('right');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), right: 'stop'});
+    } else if (e.keyCode == 40) {
+        console.log('down');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), backward: 'stop'});
+    }
+});
 });
 
 socket.on('doubles', function(doubles) {
@@ -60,4 +91,36 @@ $("#retract").click(function(){
 	socket.emit('control', {serial: $("#double-select").find(":selected").text(), kickstand: 'retract'});
 });
 
+});
+
+$(window).bind('keydown', function(e){
+    if (e.keyCode == 37) {
+        console.log('left');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), left: 'start'});
+    } else if (e.keyCode == 38) {
+        console.log('up');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), forward: 'start'});
+    } else if (e.keyCode == 39) {
+        console.log('right');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), right: 'start'});
+    } else if (e.keyCode == 40) {
+        console.log('down');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), backward: 'start'});
+    }
+});
+
+$(window).bind('keyup', function(e){
+    if (e.keyCode == 37) {
+        console.log('left');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), left: 'start'});
+    } else if (e.keyCode == 38) {
+        console.log('up');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), forward: 'start'});
+    } else if (e.keyCode == 39) {
+        console.log('right');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), right: 'start'});
+    } else if (e.keyCode == 40) {
+        console.log('down');
+	socket.emit('control', {serial: $("#double-select").find(":selected").text(), backward: 'start'});
+    }
 });
